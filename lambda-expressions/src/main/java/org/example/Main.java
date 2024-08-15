d6b5bf4c2db41d182fde6bca5131024b40212355
FishFlete <fishflete@gmail.com>
2024-08-15 08:32:54 -0400

    Update Main.java

--- lambda-expressions/src/main/java/org/example/Main.java
+++ lambda-expressions/src/main/java/org/example/Main.java
@@ -7,17 +7,32 @@ import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
+        
+        // Creates a Predicate that checks if a number is positive
         Predicate<Integer> isPositive = number -> number > 0;
+        
+        // Tests the Predicate with the value 5 and prints the result
         System.out.println("Is 5 positive? " + isPositive.test(5));
+        
+        // Tests the Predicate with the value -3 and prints the result
         System.out.println("Is -3 positive? " + isPositive.test(-3));
 
+        // Creates a Function that takes a String and returns its length
         Function<String, Integer> stringLength = str -> str.length();
+        
+        // Applies the Function to the string "Hello" and prints the length
         System.out.println("Length of 'Hello': " + stringLength.apply("Hello"));
 
+        // Creates a Consumer that prints a given message
         Consumer<String> printMessage = message -> System.out.println("Message: " + message);
+        
+        // Accepts the string "This is a lambda expression!" and prints it
         printMessage.accept("This is a lambda expression!");
 
+        // Creates a Supplier that provides a default string
         Supplier<String> defaultString = () -> "Default String";
+        
+        // Gets the value from the Supplier and prints it
         System.out.println("Supplier: " + defaultString.get());
     }
 }
